006fce067b7cb4178e45202631e63434eb01ad76
abisola <abisola.ca@gmail.com>
2021-12-22 22:08:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
-import Aos from 'aos'
+import "./App.css";
+import Aos from "aos";
 import "aos/dist/aos.css";
 
 import Loading from "./components/loading";
 
 import Home from "./pages/home";
 import MoreWorks from "./pages/more-works";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [isLoading, setIsLoading]=useState(true)
-  useEffect(()=> {
-    setTimeout(()=> {
-      setIsLoading(false)
-    }, 2000)
-    Aos.init({ duration: 1000 })
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+    Aos.init({ duration: 1000 });
+  }, []);
 
-  },[])
-
-
-  if(isLoading) {
-    return <Loading />
+  if (isLoading) {
+    return <Loading />;
   }
 
   return (
-<Router>
-<div className="App">
-  <Routes>
-  <Route path="/" exact element={<Home />} />
-  <Route path="/works" element={<MoreWorks />} />
-  </Routes>
-  
-       
-       
-      
-    </div>
-
-</Router>
- 
-
-
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/works" element={<MoreWorks />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
-
-
-
-
--- src/works.js
+++ src/works.js
@@ -1,119 +1,114 @@
-import loopstudio from './images/loopstudio.png'
-import grocery from './images/grocery.png'
-import trafalgar from './images/trafalgar.png'
-import resume from './images/resume.png'
-import weather from './images/weather.png'
-import code from './images/code.png'
-import blue from './images/blue.png'
-import fusely from './images/fusely.png'
-import converter from './images/converter.png'
-import todo from './images/todo.png'
-
-
-
+import loopstudio from "./images/loopstudio.png";
+import grocery from "./images/grocery.png";
+import trafalgar from "./images/trafalgar.png";
+import resume from "./images/resume.png";
+import weather from "./images/weather.png";
+import code from "./images/code.png";
+import blue from "./images/blue.png";
+import fusely from "./images/fusely.png";
+import converter from "./images/converter.png";
+import todo from "./images/todo.png";
 
 const works = [
-    {
-        id: 1,
-        img: todo,
-        desc: "A list-making web application",
-        addclass: "work",
-        title: "Todo WebApp",
-        github: "https://github.com/bisoladebiyi/simple-todo-list",
-        site: "https://todolist-webapp-react.herokuapp.com/",
-        tech: ["React"]
-
-    },
-    {
-        id: 2,
-        img: code,
-        desc: "A code editor for testing and showcasing HTML, CSS and JavaScript code. Built with react and tailwind",
-        addclass: "work switch",
-        title: "Code Editor",
-        github: "https://github.com/bisoladebiyi/code-editor",
-        site: "https://codeditor-react.netlify.app/",
-        tech: ["React"]
-
-    },
-    {
-        id: 3,
-        img: weather,
-        desc: "A weather app, gives basic info about the weather of all cities across the globe",
-        addclass: "work ",
-        title: "Weather App",
-        github: "https://github.com/bisoladebiyi/weather-app",
-        site: "https://bee-weather-app.herokuapp.com/",
-        tech: ["React"]
-
-    },
-    {
-        id: 4,
-        img: blue,
-        desc: "E-commerce site built with react, redux and tailwind",
-        addclass: "work switch",
-        title: "Blue Shop",
-        github: "https://github.com/bisoladebiyi/Blue-shop-website",
-        site: "https://blue-shop.netlify.app/",
-        tech: ["React", "Redux", "Tailwind"]
-
-    },
-    {
-        id: 5,
-        img: fusely,
-        desc: "A simple landing page, fully responsive. Built with HTML, SASS & Javascript.",
-        addclass: "work",
-        title: "Fusely Landing Page",
-        github: "https://github.com/bisoladebiyi/fusely",
-        site: "https://fusely-landing-page.netlify.app/",
-        tech: ["HTML", "SASS", "Javascript"]
-
-    }, {
-        id:6,
-        img: resume,
-        desc: "A single webpage resume. Fully responsive and built with React.js.",
-        addclass: "work",
-        title: "My Resume",
-        github: "https://github.com/bisoladebiyi/HNGi8-tasks-stage-2/tree/main/resume-hng-task",
-        site: "https://bisoladebiyi-resume.vercel.app/",
-        tech: ["React"]
-    }, {
-        id:7,
-        img: converter,
-        addclass: "work",
-        title: "Currency Converter",
-        github: "https://github.com/bisoladebiyi/react-currency-converter",
-        site: "https://bee-currency-converter-react.netlify.app/",
-        tech: ["React"]
-    },
-    {
-        id: 8,
-        img: grocery,
-        addclass: "work",
-        title: "Grocery Bud",
-        github: "https://github.com/bisoladebiyi/react-mini-projects/tree/main/grocery-bud",
-        site: "https://grocery-bud-bee.vercel.app/",
-        tech: ["React"]
-
-    },
-    {
-        id: 9,
-        img: trafalgar,
-        desc: "A simple landing page, fully responsive. Built with HTML, SASS & Javascript.",
-        addclass: "work switch",
-        title: "Trafalgar Landing Page",
-        github: "https://github.com/bisoladebiyi/Trafalgar-landing-page",
-        site: "https://trafalgar-bee.netlify.app/",
-        tech: ["HTML", "CSS", "Javascript"]
-
-    }, {
-        id:10,
-        img: loopstudio,
-        desc: "A single webpage resume. Fully responsive and built with React.js.",
-        addclass: "work",
-        title: "Loopsudio Landing Page",
-        github: "https://github.com/bisoladebiyi/loopstudio",
-        site: "https://loopstudio-bee.netlify.app",
-        tech: ["HTML", "CSS", "Javascript"]
-    }
-]
-export default works
+  {
+    id: 1,
+    img: todo,
+    desc: "A list-making web application",
+    addclass: "work",
+    title: "Todo WebApp",
+    github: "https://github.com/bisoladebiyi/simple-todo-list",
+    site: "https://todolist-webapp-react.herokuapp.com/",
+    tech: ["React"],
+  },
+  {
+    id: 2,
+    img: code,
+    desc: "A code editor for testing and showcasing HTML, CSS and JavaScript code. Built with react and tailwind",
+    addclass: "work switch",
+    title: "Code Editor",
+    github: "https://github.com/bisoladebiyi/code-editor",
+    site: "https://codeditor-react.netlify.app/",
+    tech: ["React"],
+  },
+  {
+    id: 3,
+    img: weather,
+    desc: "A weather app, gives basic info about the weather of all cities across the globe",
+    addclass: "work ",
+    title: "Weather App",
+    github: "https://github.com/bisoladebiyi/weather-app",
+    site: "https://bee-weather-app.herokuapp.com/",
+    tech: ["React"],
+  },
+  {
+    id: 4,
+    img: blue,
+    desc: "E-commerce site built with react, redux and tailwind",
+    addclass: "work switch",
+    title: "Blue Shop",
+    github: "https://github.com/bisoladebiyi/Blue-shop-website",
+    site: "https://blue-shop.netlify.app/",
+    tech: ["React", "Redux", "Tailwind"],
+  },
+  {
+    id: 5,
+    img: fusely,
+    desc: "A simple landing page, fully responsive. Built with HTML, SASS & Javascript.",
+    addclass: "work",
+    title: "Fusely Landing Page",
+    github: "https://github.com/bisoladebiyi/fusely",
+    site: "https://fusely-landing-page.netlify.app/",
+    tech: ["HTML", "SASS", "Javascript"],
+  },
+  {
+    id: 6,
+    img: resume,
+    desc: "A single webpage resume. Fully responsive and built with React.js.",
+    addclass: "work",
+    title: "My Resume",
+    github:
+      "https://github.com/bisoladebiyi/HNGi8-tasks-stage-2/tree/main/resume-hng-task",
+    site: "https://bisoladebiyi-resume.vercel.app/",
+    tech: ["React"],
+  },
+  {
+    id: 7,
+    img: converter,
+    addclass: "work",
+    title: "Currency Converter",
+    github: "https://github.com/bisoladebiyi/react-currency-converter",
+    site: "https://bee-currency-converter-react.netlify.app/",
+    tech: ["React"],
+  },
+  {
+    id: 8,
+    img: grocery,
+    addclass: "work",
+    title: "Grocery Bud",
+    github:
+      "https://github.com/bisoladebiyi/react-mini-projects/tree/main/grocery-bud",
+    site: "https://grocery-bud-bee.vercel.app/",
+    tech: ["React"],
+  },
+  {
+    id: 9,
+    img: trafalgar,
+    desc: "A simple landing page, fully responsive. Built with HTML, SASS & Javascript.",
+    addclass: "work switch",
+    title: "Trafalgar Landing Page",
+    github: "https://github.com/bisoladebiyi/Trafalgar-landing-page",
+    site: "https://trafalgar-bee.netlify.app/",
+    tech: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    id: 10,
+    img: loopstudio,
+    desc: "A single webpage resume. Fully responsive and built with React.js.",
+    addclass: "work",
+    title: "Loopsudio Landing Page",
+    github: "https://github.com/bisoladebiyi/loopstudio",
+    site: "https://loopstudio-bee.netlify.app",
+    tech: ["HTML", "CSS", "Javascript"],
+  },
+];
+export default works;
